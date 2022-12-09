d96056d2f69e04837c618a93187c93329ee7c1ae
codethi <docencia.thiago@gmail.com>
2022-12-09 17:23:06 -0300

    refact: connection db and port

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import pokemonsRoutes from "./routes/pokemons.routes.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(pokemonsRoutes);
 
-app.listen(5000, () => console.log("Server running in port: 5000"));
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Server running in port: ${port}`));
