e67313ebce56e061edac1c4c33761779c1fda742
codethi <docencia.thiago@gmail.com>
2022-12-09 16:02:32 -0300

    refact: before deploy

--- src/index.js
+++ src/index.js
@@ -5,5 +5,4 @@ const app = express();
 app.use(express.json());
 app.use(pokemonsRoutes);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running in port: ${port}`));
+app.listen(5000, () => console.log("Server running in port: 5000"));
